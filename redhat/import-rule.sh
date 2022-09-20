66054a656463d72eeb222b135f50778286747d61
Takuya Nakaike <nakaike@jp.ibm.com>
2022-09-20 16:03:56 +0900

    Set RHPAM password

--- redhat/import-rule.sh
+++ redhat/import-rule.sh
@@ -15,7 +15,7 @@ PORT=8443
 cd /git/dap-blueprint/src/dap_client
 
 ./dap_client.py --port $PORT wait_rhpam
-./dap_client.py --port $PORT login_rhpam admin admin
+./dap_client.py --port $PORT login_rhpam admin ${RHPAM_PASSWORD}
 ./dap_client.py --port $PORT create_space_rhpam admin --space MySpace
 sleep 10
 ./dap_client.py --port $PORT git_clone_rhpam admin ${PROJECT_NAME} "file:///git/${GIT_PROJECT_NAME}" --space MySpace
